2be68304b2c1c766a321fe8cf144b0127277debb
Sebastian Morocho <sebastianjr.morocho@gmail.com>
2025-10-19 13:46:41 +0200

    initial commit

---
+++ src/hardCodeScript.js
@@ -0,0 +1,475 @@
+const data = {
+  "departures": [
+    {
+      "destination": "Västerhaninge",
+      "direction_code": 1,
+      "direction": "Nynäshamn",
+      "state": "ATSTOP",
+      "display": "Nu",
+      "scheduled": "2025-08-17T12:21:00",
+      "expected": "2025-08-17T12:21:00",
+      "journey": {
+        "id": 2025081702831,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6161,
+        "name": "Jordbro",
+        "designation": "1"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Vega (Vardövägen)",
+      "direction_code": 2,
+      "direction": "Vega/Länna handelsplats",
+      "state": "EXPECTED",
+      "display": "1 min",
+      "scheduled": "2025-08-17T12:22:00",
+      "expected": "2025-08-17T12:22:46",
+      "journey": {
+        "id": 2025081700030,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80884,
+        "name": "Jordbro station",
+        "designation": "C"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Tungelsta",
+      "direction_code": 1,
+      "direction": "Tungelsta",
+      "state": "EXPECTED",
+      "display": "2 min",
+      "scheduled": "2025-08-17T12:21:00",
+      "expected": "2025-08-17T12:23:42",
+      "journey": {
+        "id": 2025081700029,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "designation": "D"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Bålsta",
+      "direction_code": 2,
+      "direction": "Bålsta",
+      "state": "EXPECTED",
+      "display": "2 min",
+      "scheduled": "2025-08-17T12:24:00",
+      "expected": "2025-08-17T12:24:00",
+      "journey": {
+        "id": 2025081702534,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6162,
+        "name": "Jordbro",
+        "designation": "2"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Vega station",
+      "direction_code": 2,
+      "direction": "Vega station",
+      "state": "EXPECTED",
+      "display": "8 min",
+      "scheduled": "2025-08-17T12:30:00",
+      "expected": "2025-08-17T12:30:00",
+      "journey": {
+        "id": 2025081700028,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 80813,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80829,
+        "name": "Jordbro station",
+        "designation": "A"
+      },
+      "line": {
+        "id": 837,
+        "designation": "837",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Nynäshamn",
+      "direction_code": 1,
+      "direction": "Nynäshamn",
+      "state": "EXPECTED",
+      "display": "14 min",
+      "scheduled": "2025-08-17T12:36:00",
+      "expected": "2025-08-17T12:36:00",
+      "journey": {
+        "id": 2025081702533,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6161,
+        "name": "Jordbro",
+        "designation": "1"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Kungsängen",
+      "direction_code": 2,
+      "direction": "Bålsta",
+      "state": "EXPECTED",
+      "display": "17 min",
+      "scheduled": "2025-08-17T12:39:00",
+      "expected": "2025-08-17T12:39:00",
+      "journey": {
+        "id": 2025081702836,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6162,
+        "name": "Jordbro",
+        "designation": "2"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Västerhaninge",
+      "direction_code": 1,
+      "direction": "Nynäshamn",
+      "state": "EXPECTED",
+      "display": "29 min",
+      "scheduled": "2025-08-17T12:51:00",
+      "expected": "2025-08-17T12:51:00",
+      "journey": {
+        "id": 2025081702833,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6161,
+        "name": "Jordbro",
+        "designation": "1"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Tungelsta",
+      "direction_code": 1,
+      "direction": "Tungelsta",
+      "state": "EXPECTED",
+      "display": "12:51",
+      "scheduled": "2025-08-17T12:51:00",
+      "expected": "2025-08-17T12:51:00",
+      "journey": {
+        "id": 2025081700031,
+        "state": "ASSIGNED"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "designation": "D"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Länna handelsplats",
+      "direction_code": 2,
+      "direction": "Vega/Länna handelsplats",
+      "state": "EXPECTED",
+      "display": "12:52",
+      "scheduled": "2025-08-17T12:52:00",
+      "expected": "2025-08-17T12:52:00",
+      "journey": {
+        "id": 2025081700028,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80884,
+        "name": "Jordbro station",
+        "designation": "C"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Bålsta",
+      "direction_code": 2,
+      "direction": "Bålsta",
+      "state": "EXPECTED",
+      "display": "12:54",
+      "scheduled": "2025-08-17T12:54:00",
+      "expected": "2025-08-17T12:54:00",
+      "journey": {
+        "id": 2025081702536,
+        "state": "NORMALPROGRESS",
+        "prediction_state": "NORMAL"
+      },
+      "stop_area": {
+        "id": 6161,
+        "name": "Jordbro",
+        "type": "RAILWSTN"
+      },
+      "stop_point": {
+        "id": 6162,
+        "name": "Jordbro",
+        "designation": "2"
+      },
+      "line": {
+        "id": 43,
+        "designation": "43",
+        "transport_authority_id": 1,
+        "transport_mode": "TRAIN",
+        "group_of_lines": "Pendeltåg"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Vega station",
+      "direction_code": 2,
+      "direction": "Vega station",
+      "state": "EXPECTED",
+      "display": "13:00",
+      "scheduled": "2025-08-17T13:00:00",
+      "expected": "2025-08-17T13:00:00",
+      "journey": {
+        "id": 2025081700030,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 80813,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80829,
+        "name": "Jordbro station",
+        "designation": "A"
+      },
+      "line": {
+        "id": 837,
+        "designation": "837",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Tungelsta",
+      "direction_code": 1,
+      "direction": "Tungelsta",
+      "state": "EXPECTED",
+      "display": "13:21",
+      "scheduled": "2025-08-17T13:21:00",
+      "expected": "2025-08-17T13:21:00",
+      "journey": {
+        "id": 2025081700033,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "designation": "D"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    },
+    {
+      "destination": "Vega (Vardövägen)",
+      "direction_code": 2,
+      "direction": "Vega/Länna handelsplats",
+      "state": "EXPECTED",
+      "display": "13:22",
+      "scheduled": "2025-08-17T13:22:00",
+      "expected": "2025-08-17T13:22:00",
+      "journey": {
+        "id": 2025081700034,
+        "state": "EXPECTED"
+      },
+      "stop_area": {
+        "id": 80883,
+        "name": "Jordbro station",
+        "type": "BUSTERM"
+      },
+      "stop_point": {
+        "id": 80884,
+        "name": "Jordbro station",
+        "designation": "C"
+      },
+      "line": {
+        "id": 835,
+        "designation": "835",
+        "transport_authority_id": 1,
+        "transport_mode": "BUS"
+      },
+      "deviations": []
+    }
+  ],
+  "stop_deviations": []
+};
+
+const tbodyN = document.querySelector("#depTableNorth tbody");
+const tbodyS = document.querySelector("#depTableSouth tbody");
+data.departures.forEach(dep => {
+    const row = document.createElement("tr");
+
+    if (dep.line.transport_mode == "TRAIN"){ 
+      row.innerHTML = `
+      <td>${dep.line.id}</td>
+      <td>${dep.destination}</td>
+      <td>${dep.display}</td>
+      `;
+      
+      if(dep.direction_code == 2) { //2 is for north, 1 is for south
+        tbodyN.appendChild(row) //north direction
+      } else {
+        tbodyS.appendChild(row) //south direction
+      }
+    }
+});
+
+const clockHeader = document.querySelector("#clock h1");
+function startClock() {
+  let now = new Date();
+  let hours = twoDigitNumber(now.getHours());
+  let mins = twoDigitNumber(now.getMinutes());
+  let sec = twoDigitNumber(now.getSeconds());
+  clockHeader.innerHTML = hours + ":" + mins + ":" + sec;
+  setTimeout(startClock, 1000);
+}
+
+function twoDigitNumber(number) {
+  if (number < 10) {
+    return "0" + number;
+  }
+  return number;
+}
---
+++ src/script.js
@@ -0,0 +1,98 @@
+const northTableBody = document.querySelector("#depTableNorth tbody");
+const souhtTableBody = document.querySelector("#depTableSouth tbody");
+const stationName = document.querySelector("#stationName")
+const trafficStatus = document.querySelector("#trafficStatus");
+
+const BASE_URL = "https://transport.integration.sl.se/v1/sites"
+
+// Jordbro Station id: 9729
+// Handen id: 9730
+// Skogås id: 9731
+// Vega id: 9733
+// Trångsund id: 9732
+// Farsta Strand id: 9180
+
+function fetchSites() {
+  fetch(BASE_URL + "?expand=false")
+  .then(response => response.json())
+  .then(data => {
+    // console.log('Received sites:', data);
+    // const site = data.sites.find(site => site.id === 9729); // Find Jordbro station
+    data.forEach(site => {
+      if (site.id === 9729) { // Check if the site is Jordbro station
+        console.log(site.name, "id:", site.id);
+      }
+    });
+  })
+  .catch(error => console.error('Error fetching sites:', error));
+};
+setInterval(fetchSites, 5_000);
+
+function fetchData() {
+  fetch(BASE_URL + "/9729/departures")
+  .then(response => response.json())
+  .then(data => {
+    // console.log('Received data:', data);
+    northTableBody.innerHTML = ""; //Clears north table from previous fetch cycle
+    souhtTableBody.innerHTML = ""; //Clears south table from previous fetch cycle
+
+    data.departures.forEach(dep => {
+      const row = document.createElement("tr");
+
+      if (dep.line.transport_mode == "TRAIN"){ 
+        stationName.innerHTML = `${dep.stop_area.name}`
+        document.title = `${dep.stop_area.name} Avgångar`;
+
+        // trafficStatus.innerHTML = `<h3>Trafik Status: ${dep.journey.state}</h3>`;
+        // console.log(dep.journey.prediction_state);
+        // console.log(dep.destination,": ",dep.journey);
+
+        row.innerHTML = `
+        <td colspan="1">${dep.line.id}</td>
+        <td colspan="2">${dep.destination}</td>
+        <td colspan="1">${getMinutesUntilDeparture(dep.scheduled)}</td>
+        `;
+        //<td colspan="1">${dep.display}</td>
+        
+        if(dep.direction_code == 2) { //2 is for north, 1 is for south
+          northTableBody.appendChild(row) //north direction
+        } else {
+          souhtTableBody.appendChild(row) //south direction
+        }
+      }
+});
+
+  })
+  .catch(error => console.error('Error fetching data:', error));
+}
+setInterval(fetchData, 10_000);
+
+// Gets minutes until departure
+function getMinutesUntilDeparture(depTime) {
+  const now = new Date();
+  const departureTime = new Date(depTime);
+  const diff = departureTime - now; // Difference in milliseconds
+  if (diff <= 60_000) {
+    return "Nu";
+  }
+  return Math.floor(diff / 60_000) + " min"; // Convert to minutes
+}
+
+// Clock functionality
+const clockHeader = document.querySelector("#clock");
+function startClock() {
+  let now = new Date();
+  let hours = numHandler(now.getHours());
+  let mins = numHandler(now.getMinutes());
+  let sec = numHandler(now.getSeconds());
+  clockHeader.innerHTML = hours + ":" + mins + ":" + sec;
+  setTimeout(startClock, 1_000);
+}
+
+function numHandler(number) {
+  if (number < 10) { return "0" + number; }
+  return number;
+}
+
+
+
